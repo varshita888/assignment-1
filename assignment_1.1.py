1903f9729584725838295fa7a392bd4af79b867d
varshita888 <136081074+varshita888@users.noreply.github.com>
2025-06-09 20:10:57 +0530

    Add files via upload

---
+++ assignment_1.1.py
@@ -0,0 +1,9 @@
+
+a=input('enter the first number: ')
+a=int(a)
+b=input('enter the second number: ')
+b=int(b)
+print('\nAddition: ',a+b)
+print('Subtraction: ',a-b)
+print('Multiplication: ',a*b)
+print('Division: ',a/b)
---
+++ assignment_1.2.py
@@ -0,0 +1,5 @@
+c=input('enter your first name: ')
+d=input('enter your last name: ')
+c=str(c)
+d=str(d)
+print('Hello, '+c +' '+ d+'!' +' Welcome to the Python program.')
